f1bfa288608dd13102d6e1e48f107e922922f907
quicherat <enriquevillasolis@gmail.com>
2022-08-02 13:10:40 -0500

    AddCategory Padre a Hijo

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,9 +1,31 @@
+import { useState } from "react"
+import AddCategory from "./components/AddCategory";
 
 
 const GifExpertApp = () => {
+  
+  const [categories, setCategories] = useState([ 'One Punch', 'Goku', 'Dragon Ball']);
+
+  const onAddCategory = () => {
+    setCategories([cateNueva, ...categories])
+  }
+
   return (
     <>
       <h1>GifExpertApp</h1>
+
+      <AddCategory setCategories={ setCategories } />
+      
+      <ol>
+        { categories.map(category => {
+            return(
+              <li key={category}>{category}</li>
+            )
+          })
+        }
+          
+
+      </ol>
     </>
   )
 }
