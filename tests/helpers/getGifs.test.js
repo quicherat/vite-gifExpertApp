6815cd48c1921cef5a9fbd8d9ce8657b856ea0c1
quicherat <enriquevillasolis@gmail.com>
2022-08-13 12:55:48 -0500

    Pruebas a getGifs()

---
+++ tests/helpers/getGifs.test.js
@@ -0,0 +1,18 @@
+import { getGifs } from "../../src/helpers/getGifs"
+
+
+describe('Pruebas sobre getGifs()', () => {
+  
+    test('Debe retornar un arreglo de Gifs ', async() => {
+      
+        const gifs = await getGifs('One Punch');
+        expect (gifs.length ).toBeGreaterThan(0);
+        expect (gifs[0]).toEqual({
+            id   : expect.any( String ),
+            title: expect.any( String ),
+            url  : expect.any( String )
+        })
+
+    })
+    
+})
