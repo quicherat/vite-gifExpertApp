190c9192d1243b7ea76359373838f62f38012243
quicherat <enriquevillasolis@gmail.com>
2022-08-14 16:26:50 -0500

    Pruebas a AddCategory

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import PropTypes from 'prop-types';
 
 
 export const AddCategory = ({onNewCategory}) => {
@@ -19,7 +19,7 @@ export const AddCategory = ({onNewCategory}) => {
     }
 
   return (
-    <form onSubmit={onSubmit}>
+    <form onSubmit={onSubmit} aria-label="form">
         <input
             type="text"
             placeholder="Buscar gifs"
@@ -28,6 +28,10 @@ export const AddCategory = ({onNewCategory}) => {
         />
     </form>
   )
+}
+
+AddCategory.propTypes = {
+    onNewCategory: PropTypes.func.isRequired
 }
 
 // export default AddCategory;
