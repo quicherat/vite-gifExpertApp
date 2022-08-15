2bf95e9c94bdbe755008195fccb232cd3dd4d3b3
quicherat <enriquevillasolis@gmail.com>
2022-08-15 12:01:25 -0500

    Pruebas al custom Hook useFetchGifs

---
+++ tests/hooks/useFetchGifs.test.js
@@ -0,0 +1,32 @@
+import useFetchGifs from "../../src/hooks/useFetchGifs";
+import {renderHook, waitFor} from '@testing-library/react';
+
+
+
+describe('Pruebas en el hook useFetchGifs', () => {
+  
+    test('debe regresar el estado inicial', () => {
+      
+        const { result } = renderHook( () => useFetchGifs('One Punch'));
+        const { images, isLoading} = result.current;
+
+        expect(images.length).toBe(0);
+        expect(isLoading).toBeTruthy();
+    });
+
+    test('Debe retornar un arreglo de imágenes y isLoading en false ', async() => {
+      
+        const { result } = renderHook( () => useFetchGifs('One Punch'));
+
+        await waitFor(
+            () => expect( result.current.images.length).toBeGreaterThan(0)
+        );
+
+        const { images, isLoading} = result.current;
+
+        expect(images.length).toBeGreaterThan(0);
+        expect(isLoading).toBeFalsy();
+    })
+    
+    
+})
